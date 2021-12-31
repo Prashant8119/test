861bed6b48d46b7ecc5f35f1981878bfc459173d
Prashant8119 <96903246+Prashant8119@users.noreply.github.com>
2021-12-31 12:32:01 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,69 +1,140 @@
-import pickle
+import numpy as np
+import pandas as pd
 import streamlit as st
- 
-# loading the trained model
-pickle_in = open('classifier.pkl', 'rb') 
-classifier = pickle.load(pickle_in)
- 
-@st.cache()
-  
-# defining the function which will make the prediction using the data which the user inputs 
-def prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History):   
- 
-    # Pre-processing user input    
-    if Gender == "Male":
-        Gender = 0
-    else:
-        Gender = 1
- 
-    if Married == "Unmarried":
-        Married = 0
-    else:
-        Married = 1
- 
-    if Credit_History == "Unclear Debts":
-        Credit_History = 0
-    else:
-        Credit_History = 1  
- 
-    LoanAmount = LoanAmount / 1000
- 
-    # Making predictions 
-    prediction = classifier.predict( 
-        [[Gender, Married, ApplicantIncome, LoanAmount, Credit_History]])
-     
-    if prediction == 0:
-        pred = 'Rejected'
-    else:
-        pred = 'Approved'
-    return pred
-      
-  
-# this is the main function in which we define our webpage  
-def main():       
-    # front end elements of the web page 
-    html_temp = """ 
-    <div style ="background-color:yellow;padding:13px"> 
-    <h1 style ="color:black;text-align:center;">Streamlit Loan Prediction ML App</h1> 
-    </div> 
-    """
-      
-    # display the front end aspect
-    st.markdown(html_temp, unsafe_allow_html = True) 
-      
-    # following lines create boxes in which user can enter data required to make prediction 
-    Gender = st.selectbox('Gender',("Male","Female"))
-    Married = st.selectbox('Marital Status',("Unmarried","Married")) 
-    ApplicantIncome = st.number_input("Applicants monthly income") 
-    LoanAmount = st.number_input("Total loan amount")
-    Credit_History = st.selectbox('Credit_History',("Unclear Debts","No Unclear Debts"))
-    result =""
-      
-    # when 'Predict' is clicked, make the prediction and store it 
-    if st.button("Predict"): 
-        result = prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History) 
-        st.success('Your loan is {}'.format(result))
-        print(LoanAmount)
-     
-if __name__=='__main__': 
-    main()
+import plotly.graph_objects as go
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+
+iris_data = load_iris()
+# separate the data into features and target
+features = pd.DataFrame(
+    iris_data.data, columns=iris_data.feature_names
+)
+target = pd.Series(iris_data.target)
+
+# split the data into train and test
+x_train, x_test, y_train, y_test = train_test_split(
+    features, target, test_size=0.2, stratify=target
+)
+
+
+class StreamlitApp:
+
+    def __init__(self):
+        self.model = RandomForestClassifier()
+
+    def train_data(self):
+        self.model.fit(x_train, y_train)
+        return self.model
+
+    def construct_sidebar(self):
+
+        cols = [col for col in features.columns]
+
+        st.sidebar.markdown(
+            '<p class="header-style">Iris Data Classification</p>',
+            unsafe_allow_html=True
+        )
+        sepal_length = st.sidebar.selectbox(
+            f"Select {cols[0]}",
+            sorted(features[cols[0]].unique())
+        )
+
+        sepal_width = st.sidebar.selectbox(
+            f"Select {cols[1]}",
+            sorted(features[cols[1]].unique())
+        )
+
+        petal_length = st.sidebar.selectbox(
+            f"Select {cols[2]}",
+            sorted(features[cols[2]].unique())
+        )
+
+        petal_width = st.sidebar.selectbox(
+            f"Select {cols[3]}",
+            sorted(features[cols[3]].unique())
+        )
+        values = [sepal_length, sepal_width, petal_length, petal_width]
+
+        return values
+
+    def plot_pie_chart(self, probabilities):
+        fig = go.Figure(
+            data=[go.Pie(
+                    labels=list(iris_data.target_names),
+                    values=probabilities[0]
+            )]
+        )
+        fig = fig.update_traces(
+            hoverinfo='label+percent',
+            textinfo='value',
+            textfont_size=15
+        )
+        return fig
+
+    def construct_app(self):
+
+        self.train_data()
+        values = self.construct_sidebar()
+
+        values_to_predict = np.array(values).reshape(1, -1)
+
+        prediction = self.model.predict(values_to_predict)
+        prediction_str = iris_data.target_names[prediction[0]]
+        probabilities = self.model.predict_proba(values_to_predict)
+
+        st.markdown(
+            """
+            <style>
+            .header-style {
+                font-size:25px;
+                font-family:sans-serif;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
+
+        st.markdown(
+            """
+            <style>
+            .font-style {
+                font-size:20px;
+                font-family:sans-serif;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
+        st.markdown(
+            '<p class="header-style"> Iris Data Predictions </p>',
+            unsafe_allow_html=True
+        )
+
+        column_1, column_2 = st.beta_columns(2)
+        column_1.markdown(
+            f'<p class="font-style" >Prediction </p>',
+            unsafe_allow_html=True
+        )
+        column_1.write(f"{prediction_str}")
+
+        column_2.markdown(
+            '<p class="font-style" >Probability </p>',
+            unsafe_allow_html=True
+        )
+        column_2.write(f"{probabilities[0][prediction[0]]}")
+
+        fig = self.plot_pie_chart(probabilities)
+        st.markdown(
+            '<p class="font-style" >Probability Distribution</p>',
+            unsafe_allow_html=True
+        )
+        st.plotly_chart(fig, use_container_width=True)
+
+        return self
+
+
+sa = StreamlitApp()
+sa.construct_app()
